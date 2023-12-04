a707c7e0eb5b7d706036aa0c5501c8511fccd6c1
dhelafrank <ubongabasijerome8@gmail.com>
2023-12-04 14:47:47 +0100

    Fixed a couple bugs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const {template} = require('./data/mail-template')
 
 
 // Function to select and delete a random message
-const selectAndDeleteRandomMessage = (recipient Address, mailSubject) => {
+const selectAndDeleteRandomMessage = (recipientAddress, mailSubject) => {
   if (messages.length > 0) {
     const randomIndex = Math.floor(Math.random() * messages.length);
     const selectedMessage = messages.splice(randomIndex, 1)[0];
@@ -13,7 +13,7 @@ const selectAndDeleteRandomMessage = (recipient Address, mailSubject) => {
 
     console.log(`Selected message: ${selectedMessage.message} by ${selectedMessage.author}`);
 
-sendMail(intendedAddress, mailSubject, template(selectedMessage.message, selectedMessage.author))
+sendMail(recipientAddress, mailSubject, template(selectedMessage.message, selectedMessage.author))
 
 //Other functions could be added here
 
@@ -27,6 +27,6 @@ cron.schedule('0 0 * * *', () => {
   selectAndDeleteRandomMessage("recipient@example.com", "Auto Mail Sender");
 });
 
-
-//selectAndDeleteRandomMessage("recipient@example.com", "Auto Mail Sender");
+console.log("Script is running...");
+// selectAndDeleteRandomMessage("recipient@example.com", "Auto Mail Sender");
 //module.exports = {selectAndDeleteRandomMessage}
--- services/mail-sender.js
+++ services/mail-sender.js
@@ -3,6 +3,7 @@ const {
 } = require("./mail-transporter")
 
 const sendMail = async (address, subject, content) => {
+    console.log(`Sending mail to ${address}`);
     try {
         const mailOptions = {
             from: process.env.USER_EMAIL,
