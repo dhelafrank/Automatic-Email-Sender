b488add0e76a395715a28778bbc2b5072f0cbaa4
Ubongabasi Jerome <87214555+dhelafrank@users.noreply.github.com>
2023-12-04 08:14:49 +0100

    initialized mail sending module

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const cron = require('node-cron');
 const { messages } = require('./data/messages')
-const {sendMail} = require(./services/mail-sender)
+const {sendMail} = require('./services/mail-sender')
+const {template} = require('./data/mail-template')
+
 
 // Function to select and delete a random message
-const selectAndDeleteRandomMessage = () => {
+const selectAndDeleteRandomMessage = (intendedAddress, mailSubject) => {
   if (messages.length > 0) {
     const randomIndex = Math.floor(Math.random() * messages.length);
     const selectedMessage = messages.splice(randomIndex, 1)[0];
 
 
     console.log(`Selected message: ${selectedMessage.message} by ${selectedMessage.author}`);
+
+sendMail(intendedAddress, mailSubject, template(selectedMessage.message, selectedMessage.author))
+
 
 
 //Other functions could be added here
