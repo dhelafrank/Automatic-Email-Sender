1f3ac62aa0b927886c40504b291da307caad9ef9
Ubongabasi Jerome <87214555+dhelafrank@users.noreply.github.com>
2023-12-04 07:44:05 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("getting started")
+const cron = require('node-cron');
+let { messages } = require('./messages')
+
+// Function to select and delete a random message
+const selectAndDeleteRandomMessage = () => {
+  if (messages.length > 0) {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const selectedMessage = messages.splice(randomIndex, 1)[0];
+
+
+    console.log(`Selected message: ${selectedMessage.message} by ${selectedMessage.author}`);
+
+
+//Other functions could be added here
+
+  } else {
+    console.log('No more messages to select.');
+  }
+};
+
+// Scheduling the cron job to run every day at midnight
+cron.schedule('0 0 * * *', () => {
+  selectAndDeleteRandomMessage();
+});
+
+
+//selectAndDeleteRandomMessage()
