5b0fb821f13e9a40512ac27bee87abbe8618351c
Ubongabasi Jerome <87214555+dhelafrank@users.noreply.github.com>
2023-12-04 08:05:20 +0100

    Created messages.js in /data

---
+++ data/messages.js
@@ -0,0 +1,11 @@
+const messages = [
+  {"message": "You bring brightness to my life.", "author": "Anonymous"},
+  {"message": "Every moment spent with you is reminiscent of a dream.", "author": "Unknown"},
+  {"message": "The affection you share is a valued treasure.", "author": "Anonymous"},
+
+  // ... Additional messages can be included here
+
+  {"message": "I am eternally thankful for the bond we share.", "author": "Unknown"}
+];
+
+module.exports = { messages };
