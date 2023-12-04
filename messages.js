c2cdf92229e2d3993fee8e9b99457f510d9a6cac
Ubongabasi Jerome <87214555+dhelafrank@users.noreply.github.com>
2023-12-04 07:46:20 +0100

    Created messages.js

---
+++ messages.js
@@ -0,0 +1,11 @@
+let messages = [
+  {"message": "You are the sunshine in my life.", "author": "Anonymous"},
+  {"message": "Every moment with you feels like a dream.", "author": "Unknown"},
+  {"message": "Your love is my greatest treasure.", "author": "Anonymous"},
+
+  // ... You can add more messages here
+
+  {"message": "Forever grateful for the love we share.", "author": "Unknown"}
+]
+
+module.exports = {messages}
